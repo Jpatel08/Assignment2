a1ef639963665325913389a20bfc8f19745b2cd2
Jpatel08 <jaypatel515121@gmail.com>
2023-04-06 13:41:22 -0500

    Updated app

--- assignment2/src/index.js
+++ assignment2/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { products } from './products';
-import { Categories } from "./Categories";
+import { Categories } from "./categories";
 
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
