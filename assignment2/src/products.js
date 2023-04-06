fc267a442395f841452028f1c0ae9ac31548808d
Jpatel08 <jaypatel515121@gmail.com>
2023-04-06 13:39:18 -0500

    updated image paths

--- assignment2/src/products.js
+++ assignment2/src/products.js
@@ -1,4 +1,4 @@
-export const products = [
+export const Products = [
   {
     "id": 1,
     "title": "Hoka Clifton 9",
@@ -6,7 +6,7 @@ export const products = [
     "description": "The ninth generation of the Hoka Clifton line with a new outsole design and added support makes for an extra soft ride to go the distance.",
     "category": "Hoka",
     "rating": {"rate": 4.75, "count": 241},
-    "image": require("./hokashoe.jpg")
+    "image": require("./images/hokashoe.jpg")
     
   },
   {
@@ -16,7 +16,7 @@ export const products = [
     "description": "Experience a softer feel with the GEL-CUMULUS® 25. This  everyday trainer is a great choice for various workouts and distances.",
     "category": "Asic",
     "rating": {"rate": 4.3, "count": 9},
-    "image": require("asicshoe.jpeg")
+    "image": require("./images/asicsshoe.webp")
   },
   {
     "id": 3,
@@ -25,7 +25,7 @@ export const products = [
     "description": "With a soft feel, smooth ride, and trusted fit, the men's Ghost 15 delivers the comfort and performance runners want.",
     "category": "Brooks",
     "rating": {"rate": 4.5, "count": 360},
-    "image": require("brooksshoe.jpeg")
+    "image": require("./images/brooksshoe1.png")
   },
   {
     "id": 4,
@@ -34,7 +34,7 @@ export const products = [
     "description": "The smooth transitions of the pinnacle underfoot cushioning experience are fine-tuned with updated midsole mapping, which applies more foam to wider areas of the midsole and increases flexibility ",
     "category": "New Balance",
     "rating": {"rate": 4.7, "count": 1021},
-    "image": require("newbalanceshoe.jpeg")
+    "image": require("./images/newbalanceshoe.jpeg")
   },
   {
     "id": 5,
@@ -43,15 +43,15 @@ export const products = [
     "description": "Once you take a few strides in the Nike Air Zoom Alphafly NEXT% 2, you'll never look at your favorite pair of old racing shoes the same way again. These rocket ships are made to help shave precious time off your personal records without surrendering the foundation you need to go the full distance. ",
     "category": "Nike",
     "rating": {"rate": 5, "count": 12},
-    "image": require("nikeshoe1.jpeg")
+    "image": require("./images/nikeshoe1.jpeg")
   },
   {
     "id": 6,
-    "title": "Nike Infinity Run 3",
+    "title": "Nike Invincible Run 3",
     "price": 275,
-    "description": "With soft and supportive cushioning, built to help keep you on the run, the Infinity React 3 is here to help get you moving Regardless of whether your pace is snail-like or lickety-split. ",
+    "description": "With maximum cushioning to support every mile, the Invincible 3 gives you our highest level of comfort underfoot to help you stay on your feet today, tomorrow and beyond.",
     "category": "Nike",
     "rating": {"rate": 4.8, "count": 124},
-    "image": require("nikeshoe2.jpeg")
+    "image": require("./images/nikeshoe2.png")
   },
 ]
